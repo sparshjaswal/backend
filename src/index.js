dfc13f4dde8c9639f92d961b733d75fc54cc2e23
sparsh jaswal <jaswalsparsh@gmail.com>
2024-08-06 23:34:08 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-// app.js
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
+import mongoose from "mongoose";
 import routes from "./routes/appRoutes.js";
 
 const app = express();
-
 app.use(bodyParser.json());
-app.use(cors());
 
-// Routes will go here
+// Connect to the specific database and collection
+const mongoUrl =
+  "mongodb+srv://jaswalsparsh:sparshjaswal123@cluster0.iiyph.mongodb.net/backend?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB - backend database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  });
+
+// Access to the database through app.locals
+const db = mongoose.connection;
+app.locals.db = db;
+
+// Error handling middleware (example)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Routes
 app.use("/api/v1/", routes);
 
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-  
 });
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,20 @@
-// models/User.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
-
 export default User;
